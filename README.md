26b39f2e073bbca46a9153c66795107c49f18f68
E <sh0oki@users.noreply.github.com>
2023-10-06 00:04:49 +0300

    Updates for the new rail.co.il API

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Israel Rail API
+
 [![image](https://img.shields.io/pypi/v/israel-rail-api.svg)](https://pypi.org/project/israel-rail-api/)
 [![image](https://img.shields.io/pypi/l/israel-rail-api.svg)](https://pypi.org/project/israel-rail-api/)
 [![Build Status](https://travis-ci.org/sh0oki/israel-rail-api.svg?branch=master)](https://travis-ci.org/sh0oki/israel-rail-api)
@@ -8,19 +9,21 @@ This is a unofficial wrapping of the API of Israeli Rail network in Python.
 Use this script for checking your own train schedule, integrating with Alexa, and so on!
 
 ## Installing
-Tested with Python 3.6, should probably work fine with other versions too.
 
-        pip install israel-rail-api
+Tested with Python 3.11, should probably work fine with other versions too.
+
+    pip install israel-rail-api
 
 ## Usage
-        import israelrailapi
-        s = israelrailapi.TrainSchedule()
-        print(s.query('Tel Aviv University', 'Jerusalem Yitzhak Navon'))
-        
+
+    import israelrailapi
+    s = israelrailapi.TrainSchedule()
+    print(s.query('Tel Aviv University', 'Jerusalem Yitzhak Navon'))
+
 A simple test, to make sure the script is working is included in `schedule.py`:
 
-        python schedule.py "tel aviv hahagana" "ben gurion airport" 2018-12-24 0900 
-        
+    python3 schedule.py "tel aviv hahagana" "ben gurion airport" 2023-06-24 0900 
 
 ## Contributing
-We'd love your contributions! Fire up a (tested!) Pull request and we'll be happy to approve it.
+
+We'd love your contributions! Fire up a (tested!) Pull request, and we'll be happy to approve it.
--- israelrailapi/schedule.py
+++ israelrailapi/schedule.py
@@ -1,13 +1,14 @@
 import logging
-import time
 import sys
+import time
 
-from israelrailapi.train_station import translate_station
-from israelrailapi.api import GetRoutesApi
+from api import GetRoutesApi
+from train_station import translate_station
 
 
 class TrainSchedule(object):
-    def query(self, src_station, dst_station, start_date=None, start_hour=None):
+    @staticmethod
+    def query(src_station, dst_station, start_date=None, start_hour=None):
         src_station = translate_station(src_station)
         dst_station = translate_station(dst_station)
         if start_date is None:
@@ -15,12 +16,14 @@ class TrainSchedule(object):
         if start_hour is None:
             start_hour = "09:00"
         start_date = start_date.strip().replace('-', '').replace('/', '').replace('.', '')
-        start_hour = start_hour.strip().replace(':', '')
+        start_hour = start_hour.strip().replace(':', '').replace('-', '')
+        start_hour = f"{start_hour[:2]}:{start_hour[2:]}"
+        start_date = f"{start_date[:4]}-{start_date[4:6]}-{start_date[6:8]}"
 
         logging.info("Query: %s->%s (%s %s)" % (src_station, dst_station,
                                                 start_date, start_hour))
-        result = GetRoutesApi().request(OId=src_station, TId=dst_station,
-                                        Date=start_date, Hour=start_hour)
+        result = GetRoutesApi().request(fromStation=src_station, toStation=dst_station,
+                                        date=start_date, hour=start_hour)
         logging.info(result)
         return result
 
@@ -28,7 +31,8 @@ class TrainSchedule(object):
 if '__main__' == __name__:
     logging.basicConfig(level=logging.DEBUG)
     t = TrainSchedule()
-    if len(sys.argv) >= 4:
-        t.query(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    if len(sys.argv) == 5:
+        q = t.query(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        print(q)
     else:
-        print("Usage: %s <src> <dst> <YYYY-MM-DD> <hh:mm>" % (sys.argv[0], ))
+        print("Usage: %s <src> <dst> <YYYY-MM-DD> <hh:mm>" % (sys.argv[0],))
